6feda4271be7fabe847c3473a510c4ca9e48e799
Yash Bansal <yashbansal.2099@gmail.com>
2025-03-11 14:15:39 +0530

    "modified:   A_118.java"

--- A_118.java
+++ A_118.java
@@ -11,7 +11,7 @@ public class A_118 {
             if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'|| c=='A' || c=='E' || c=='I' || c=='O' || c=='U' || c=='y' || c=='Y'){
                 continue;
             }
-            res.append("."+Character.toLowerCase(c));
+            res.append('.').append(Character.toLowerCase(c));
         }
         System.out.println(res.toString());
     }
