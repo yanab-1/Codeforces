b2a5ac3e6e081b6c25cb695a100908d34a9b6b44
Yash Bansal <yashbansal.2099@gmail.com>
2025-04-11 13:28:18 +0530

    daily commit

--- B_1828.java
+++ B_1828.java
@@ -7,10 +7,16 @@ public class B_1828 {
         while(t-->0){
             int n=sc.nextInt();
             int[] a=new int[n];
+            int res=0;
             for(int i=0;i<n;i++){
                 a[i]=sc.nextInt();
+                res=gcd(res,Math.abs(a[i]-i-1));
             }
-            
+            System.out.println(res);
         }
+    }
+    public static int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
     }
 }
