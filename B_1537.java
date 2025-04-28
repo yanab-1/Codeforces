d0dda3993fa910d0c3ec1cab66841aadd89839cc
Yash Bansal <yashbansal.2099@gmail.com>
2025-04-29 00:05:12 +0530

    daily commit

---
+++ B_1537.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class B_1537 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int i=sc.nextInt();
+            int j=sc.nextInt();
+            System.out.println(1+" "+1+" "+n+" "+m);
+        }
+    }
+}
