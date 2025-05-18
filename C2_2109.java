ba3bff24f5df12bcbc2e6ee0c4e54b32ea1ec276
Yash Bansal <yashbansal.2099@gmail.com>
2025-05-18 12:57:52 +0530

    daily commit

---
+++ C2_2109.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class C2_2109 {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            System.out.println("mul "+9);
+            System.out.flush();
+            int res=sc.nextInt();
+            System.out.println("digit");
+            System.out.flush();
+            res=sc.nextInt();
+            System.out.println("digit");
+            System.out.flush();
+            res=sc.nextInt();
+            System.out.println("add "+(n-9));
+            System.out.flush();
+            res=sc.nextInt();
+            System.out.println("!");
+            System.out.flush();
+            int ans=sc.nextInt();
+        }
+    }
+}
