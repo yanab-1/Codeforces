6832420141983f62ad3c56a05597fdf7ef8bf4d9
Yash Basnal <yashbansal.2099@gmail.com>
2026-03-26 23:45:30 +0530

    daily commit

--- Questions_2/E_61.java
+++ Questions_2/E_61.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class E_61 {
@@ -6,13 +10,13 @@ public class E_61 {
         solve(sc);
     }
     static class Segment {
-        int[] tree;
+        long[] tree;
         int n;
         int[] arr;
 
         Segment(int n, int[] arr){
             this.n = n;
-            tree = new int[4 * n];
+            tree = new long[4 * n];
             this.arr = arr;
             build(0, 0, n - 1);
         }
@@ -28,11 +32,11 @@ public class E_61 {
             tree[idx] = tree[2 * idx + 1] + tree[2 * idx + 2]; // we change logic here for MAX, MIN, GCD
         }
 
-        public int query(int l, int r){
+        public long query(int l, int r){
             return query(l, r, 0, 0, n - 1);
         }
 
-        private int query(int l, int r, int idx, int st, int end){
+        private long query(int l, int r, int idx, int st, int end){
             // no overlap
             if(r < st || l > end){
                 return 0;
@@ -45,8 +49,8 @@ public class E_61 {
 
             // partial overlap
             int mid = (st + end) / 2;
-            int left = query(l, r, 2 * idx + 1, st, mid);
-            int right = query(l, r, 2 * idx + 2, mid + 1, end);
+            long left = query(l, r, 2 * idx + 1, st, mid);
+            long right = query(l, r, 2 * idx + 2, mid + 1, end);
             return left + right; // change according to question
         }
 
@@ -56,7 +60,7 @@ public class E_61 {
 
         private void point_Update(int i, int val, int l, int r, int idx){
             if(l == r){
-                tree[idx] = val;
+                tree[idx] += val;
                 return;
             } 
             int mid = (l + r) / 2;
@@ -73,10 +77,41 @@ public class E_61 {
     private static void solve(Scanner sc) {
         int n = sc.nextInt();
         int[] a = new int[n];
+        int[] c = new int[n];
         for(int i = 0; i < n; i++){
             a[i] = sc.nextInt();
+            c[i] = a[i];
         }
-        Segment sg = new Segment(n, a);
-        
+        Arrays.sort(c);
+        List<Integer> uniqList = new ArrayList<>();
+        uniqList.add(c[0]);
+        for(int i = 1; i < n; i++){
+            if(c[i] != c[i - 1]){
+                uniqList.add(c[i]);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            a[i] = Collections.binarySearch(uniqList, a[i]);
+        }
+        int max = -1;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max, a[i]);
+        }
+        max++;
+        int[] freq = new int[max];
+        for(int i : a){
+            freq[i]++;
+        }
+        Segment sg1 = new Segment(max, freq);
+        Segment sg2 = new Segment(max, new int[max]);
+        sg1.point_Update(a[0], -1);
+        sg2.point_Update(a[0], 1);
+        long count = 0;
+        for(int i = 1; i < n - 1; i++){
+            sg1.point_Update(a[i], -1);
+            sg2.point_Update(a[i], 1);
+            count += sg1.query(0, a[i] - 1) * sg2.query(a[i] + 1, max - 1);
+        }
+        System.out.println(count);
     }   
 }
