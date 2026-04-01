2927b3e8ab4bba014703cae1a618110729968c03
Yash Basnal <yashbansal.2099@gmail.com>
2026-04-02 00:17:03 +0530

    daily commit

--- Questions_2/C_2147.java
+++ Questions_2/C_2147.java
@@ -10,6 +10,28 @@ public class C_2147 {
     private static void solve(Scanner sc) {
         int n = sc.nextInt();
         String s = sc.next();
-        
+        boolean ok = true;
+        boolean curr = (s.charAt(0) == '1');
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0')
+                cnt++;
+
+            if (i == 0)
+                continue;
+
+            if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == '0')
+                curr = false;
+
+            if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == '1') {
+                if (curr && cnt % 2 == 1)
+                    ok = false;
+                curr = true;
+                cnt = 0;
+            }
+        }
+        if (curr && cnt % 2 == 1 && s.charAt(n - 1) == '1')
+            ok = false;
+        System.out.println(ok ? "YES" : "NO");
     }
 }
