d4a1332efcaba443bdf8123e43b8058d41534979
Yash Basnal <yashbansal.2099@gmail.com>
2026-04-20 00:35:24 +0530

    daily commit

--- Questions_2/E_2200.java
+++ Questions_2/E_2200.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class E_2200 {
     public static void main(String[] args) {
@@ -10,6 +11,52 @@ public class E_2200 {
     }
 
     private static void solve(Scanner sc) {
-        
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = prime(a[i]);
+        }
+        if (isSorted(a)) {
+            System.out.println("Bob");
+        } else if (minElement(b) == -1) {
+            System.out.println("Alice");
+        } else if (isSorted(b)) {
+            System.out.println("Bob");
+        } else {
+            System.out.println("Alice");
+        }
+    }
+
+    private static int prime(int x) {
+        TreeSet<Integer> s = new TreeSet<>();
+        for (int i = 2; i * i <= x; i++) {
+            while (x % i == 0) {
+                s.add(i);
+                x /= i;
+            }
+        }
+        if (x > 1) s.add(x);
+        if (s.size() > 1) return -1;
+        if (s.size() == 0) return 1;
+        return s.first();
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) return false;
+        }
+        return true;
+    }
+
+    private static int minElement(int[] arr) {
+        int min = arr[0];
+        for (int x : arr) {
+            min = Math.min(min, x);
+        }
+        return min;
     }
 }
